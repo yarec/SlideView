f1fcda9a04cc2caf18480d9cd2aa21973ea1fb69
yarec <yarec@163.com>
2015-05-14 10:39:45 +0800

    carthage

---
+++ SlideView/SlideView/SlideView.h
@@ -0,0 +1,20 @@
+//
+//  SlideView.h
+//  SlideView
+//
+//  Created by r.t on 15/5/14.
+//  Copyright (c) 2015年 r.t. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SlideView.
+FOUNDATION_EXPORT double SlideViewVersionNumber;
+
+//! Project version string for SlideView.
+FOUNDATION_EXPORT const unsigned char SlideViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SlideView/PublicHeader.h>
+
+
+#import "MainScrollViewController.h"
